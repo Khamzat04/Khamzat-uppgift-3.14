df0fb99941cd7614116f51b8a88b35c2de5e8f2a
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-30 09:56:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+//Uppgift 3.14
+
+public class Program
+{
+	public static void Main()
+	{
+		// proggrammet frågar om Hen har gått ut gymnasium om hen svarar ja och är under 22 år då säger programmet vi vill gärna anställa dig.
+		Console.WriteLine("har du gått ut gymnasiet? j/n ");
+		string svar = Console.ReadLine().ToLower();
+		Console.WriteLine("Hur gammal är du");
+		int ålder = int.Parse(Console.ReadLine());
+		if (svar == "j" && ålder <= 22 && ålder >= 19)
+		{
+			Console.WriteLine("vi vill gärna anställa dig  ");
+		}
+
+		else
+		{
+			Console.WriteLine("Vi letar tyvärr efter annan personal just nu. ");
+		}
+
+
+
+
+	}
+}
